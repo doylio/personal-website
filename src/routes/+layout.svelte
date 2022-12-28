5ff2dac5428cfbdaf8fd11d5a7de3beea4a035e9
doylio <34788350+doylio@users.noreply.github.com>
2022-12-28 10:54:12 -0500

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { inject } from '@vercel/analytics';
+
+	inject();
 </script>
 
 <SvelteToast />
